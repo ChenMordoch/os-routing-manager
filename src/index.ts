e948cdb7f3de271f5b96312ce3de0c280f99b811
Hen Mordoch <hen.mord@gmail.com>
2020-11-16 00:13:03 +0200

    added sync

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Server } from "./server";
 import express from "express";
+import { RequestsDbHandler } from './db/requestsDbHandler'
 
 const app = express();
 const PORT = 8080;
 
 const server = new Server(app);
+new RequestsDbHandler().createDb();
 server.start(PORT);
